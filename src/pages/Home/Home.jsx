189ac384fa05b0948af38b54a9c046227d6d82e9
Hashadhi Jayasinghe <hashadhijayasinghe@gmail.com>
2025-04-28 16:21:29 -0700

    Add Hero Section1 in Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import Section1 from './Section1';
 
 function Home() {
   return (
     <Layout>
-        <div> Home </div>
+
+      {/* Home Section Hero Banner */}
+        <Section1 />
+
     </Layout>
   )
 }
 
-export default Home
+export default Home;
---
+++ src/pages/Home/Section1.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Door from '../../assets/1.png';
+import { Link } from 'react-router-dom';
+
+function Section1() {
+  return (
+    <section className='hero-section'>
+        <Container>
+            <Row>
+                <Col lg={7} className="mb-lg-5">
+                    <div className="position-relative">
+                        <img src={Door} className='img-fluid' alt="Door" />
+                    </div>
+                </Col>
+                <Col lg={5}>
+                    <div className="hero-text text-center">
+                        <h1 className='text-white'>Door Name</h1>
+                        <h2 className='text-white>'>Aluminium</h2>
+                        <p className='text-white pt-2 pb-4'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, ut nesciunt saepe voluptas blanditiis suscipit quaerat vel possimus voluptate atque inventore dolorem ab facilis optio perferendis. Ad dignissimos suscipit commodi.
+                        </p>
+                        <Link to="/" className="btn call_now">Call Now</Link>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+  )
+}
+
+export default Section1;
